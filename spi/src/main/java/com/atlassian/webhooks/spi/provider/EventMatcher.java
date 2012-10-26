df2e6bc478bd4e75ddb8d1049e4ea0bcec6147ab
Samuel Le Berrigaud <sam@leberrigaud.org>
2012-10-26 14:52:30 +0200

    Update javadocs + formatting.

--- spi/src/main/java/com/atlassian/webhooks/spi/provider/EventMatcher.java
+++ spi/src/main/java/com/atlassian/webhooks/spi/provider/EventMatcher.java
@@ -5,7 +5,8 @@ package com.atlassian.webhooks.spi.provider;
  */
 public interface EventMatcher<T>
 {
-    EventMatcher<Object> ALWAYS_TRUE = new EventMatcher<Object>() {
+    static final EventMatcher<Object> ALWAYS_TRUE = new EventMatcher<Object>()
+    {
         @Override
         public boolean matches(Object event, String pluginKey)
         {
@@ -16,13 +17,21 @@ public interface EventMatcher<T>
     /**
      * Useful for registering events but then firing them manually outside the event system
      */
-    EventMatcher<Object> ALWAYS_FALSE = new EventMatcher<Object>() {
-            @Override
-            public boolean matches(Object event, String pluginKey)
-            {
-                return false;
-            }
-        };
+    static final EventMatcher<Object> ALWAYS_FALSE = new EventMatcher<Object>()
+    {
+        @Override
+        public boolean matches(Object event, String pluginKey)
+        {
+            return false;
+        }
+    };
 
+    /**
+     * Tells whether the fired event matches the web hook registration.
+     *
+     * @param event the event being fired, associated to the web hook
+     * @param pluginKey the key of the plugin which listens to the web hook.
+     * @return {@code true} if this event matches the web hook registration, {@code false} otherwise.
+     */
     boolean matches(T event, String pluginKey);
 }
