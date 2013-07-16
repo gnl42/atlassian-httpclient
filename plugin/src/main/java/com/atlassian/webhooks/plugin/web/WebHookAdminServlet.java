b0f2b88bf192f3ed15947569afbcf81b1ba239a9
Filip Rogaczewski <frogaczewski@atlassian.com>
2013-07-16 15:28:32 +0200

    WEBHOOKS-30: Is user admin condition added

--- plugin/src/main/java/com/atlassian/webhooks/plugin/web/WebHookAdminServlet.java
+++ plugin/src/main/java/com/atlassian/webhooks/plugin/web/WebHookAdminServlet.java
@@ -2,6 +2,7 @@ package com.atlassian.webhooks.plugin.web;
 
 import com.atlassian.plugin.webresource.WebResourceManager;
 import com.atlassian.sal.api.message.I18nResolver;
+import com.atlassian.sal.api.user.UserManager;
 import com.atlassian.templaterenderer.TemplateRenderer;
 import com.atlassian.webhooks.plugin.web.util.HtmlSafeContent;
 import com.atlassian.webhooks.plugin.web.util.RendererContextBuilder;
@@ -17,16 +18,26 @@ import static com.google.common.base.Preconditions.checkNotNull;
 
 public class WebHookAdminServlet extends HttpServlet
 {
-    private static final String TEMPLATE_PATH = "templates/webhooks-admin.vm";
+    private static final String WEBHOOKS_ADMIN_TEMPLATE_PATH = "templates/webhooks-admin.vm";
+    private static final String NO_ADMIN_PRIVILEGES_TEMPLATE_PATH = "templates/no_admin_privileges.vm";
+    private static final String TEXT_HTML_CHARSET_UTF_8 = "text/html; charset=utf-8";
 
     private final TemplateRenderer templateRenderer;
     protected final WebResourceManager webResourceManager;
     private final I18nResolver i18nResolver;
     private final WebHookUIRegistry webHookUIRegistry;
+    private final UserManager userManager;
 
-    public WebHookAdminServlet(TemplateRenderer templateRenderer, WebResourceManager webResourceManager, I18nResolver i18nResolver, WebHookUIRegistry webHookUIRegistry)
+    public WebHookAdminServlet(
+            TemplateRenderer templateRenderer,
+            WebResourceManager webResourceManager,
+            I18nResolver i18nResolver,
+            WebHookUIRegistry webHookUIRegistry,
+            UserManager userManager)
     {
-        this.webHookUIRegistry = webHookUIRegistry;
+        System.out.println("enablling webhooks");
+        this.webHookUIRegistry = checkNotNull(webHookUIRegistry);
+        this.userManager = checkNotNull(userManager);
         this.templateRenderer = checkNotNull(templateRenderer);
         this.webResourceManager = checkNotNull(webResourceManager);
         this.i18nResolver = checkNotNull(i18nResolver);
@@ -35,11 +46,24 @@ public class WebHookAdminServlet extends HttpServlet
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException
     {
-        render(req, resp);
-//        super.doGet(req, resp);    //To change body of overridden methods use File | Settings | File Templates.
+        if (userManager.isAdmin(userManager.getRemoteUsername()))
+        {
+            render(resp);
+        }
+        else
+        {
+            renderNoAdminPrivileges(resp);
+        }
     }
 
-    private void render(HttpServletRequest req, HttpServletResponse response) throws IOException
+    private void renderNoAdminPrivileges(HttpServletResponse response) throws IOException
+    {
+        response.setContentType(TEXT_HTML_CHARSET_UTF_8);
+        response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+        templateRenderer.render(NO_ADMIN_PRIVILEGES_TEMPLATE_PATH, response.getWriter());
+    }
+
+    private void render(HttpServletResponse response) throws IOException
     {
         webResourceManager.requireResourcesForContext("atl.plugins.webhooks.admin");
         final RendererContextBuilder builder = new RendererContextBuilder()
@@ -55,8 +79,8 @@ public class WebHookAdminServlet extends HttpServlet
                 })
                 .put("webResourceManager", webResourceManager)
                 .put("webHookUIRegistry", webHookUIRegistry);
-        response.setContentType("text/html; charset=utf-8");
-        templateRenderer.render(TEMPLATE_PATH, builder.build(), response.getWriter());
+        response.setContentType(TEXT_HTML_CHARSET_UTF_8);
+        templateRenderer.render(WEBHOOKS_ADMIN_TEMPLATE_PATH, builder.build(), response.getWriter());
     }
 
 }
