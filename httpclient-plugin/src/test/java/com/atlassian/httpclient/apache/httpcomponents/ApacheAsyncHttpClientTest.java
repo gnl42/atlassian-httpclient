5d5c606043e86f3df0a93b165f090de4fc435b10
Samuel Le Berrigaud <sam@leberrigaud.org>
2014-02-13 11:25:23 +0100

    Remove sysout that was added for Bamboo debug.

--- httpclient-plugin/src/test/java/com/atlassian/httpclient/apache/httpcomponents/ApacheAsyncHttpClientTest.java
+++ httpclient-plugin/src/test/java/com/atlassian/httpclient/apache/httpcomponents/ApacheAsyncHttpClientTest.java
@@ -33,7 +33,6 @@ public final class ApacheAsyncHttpClientTest
     @Before
     public void setUp() throws Exception
     {
-        System.out.println("JAVA_HOME:" + System.getProperty("java.home"));
         server = HttpsServer.create(new InetSocketAddress(8000), 0);
         server.setHttpsConfigurator(new HttpsConfigurator(getSslContext()));
         server.createContext("/", new NoOpHandler());
