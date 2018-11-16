83c72b0b0e545f324bef2bad9ff3c6c71caacc98
Bryan Turner <bturner@atlassian.com>
2018-11-16 13:14:05 -0800

    BSP-83 Upgrade AMPS, platform and refapp milestones.
    - Reenabled AsyncHttpClientPluginTest, since Refapp now provides
      io.atlassian.fugue and io.atlassian.util.concurrent
    - Updated to AMPS 8.0.0-m6 for Java 11 support
    - Updated platform and Refapp milestones

--- httpclient-plugin/src/test/java/it/com/atlassian/httpclient/apache/httpcomponents/AsyncHttpClientPluginTest.java
+++ httpclient-plugin/src/test/java/it/com/atlassian/httpclient/apache/httpcomponents/AsyncHttpClientPluginTest.java
@@ -16,7 +16,6 @@ import org.apache.http.impl.auth.BasicScheme;
 import org.apache.http.impl.client.BasicAuthCache;
 import org.apache.http.impl.client.BasicCredentialsProvider;
 import org.apache.http.impl.client.HttpClientBuilder;
-import org.junit.Ignore;
 import org.junit.Test;
 
 import java.io.IOException;
@@ -28,12 +27,6 @@ import static org.hamcrest.Matchers.is;
 /**
  * Tests that we can start the plugin.
  */
-
-/* We can't anymore :/
- * Now we need io.fugue instead of com.fugue so this will not work until we will have a new shiny refapp
- */
-//TODO: unignore
-@Ignore
 public class AsyncHttpClientPluginTest {
     @Test
     public void testPluginStarts() throws IOException {
